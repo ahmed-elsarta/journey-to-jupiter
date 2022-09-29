777a51aa6f38dbf92f808da07171ec812d6f5e54
michaelhany510 <69060386+michaelhany510@users.noreply.github.com>
2022-09-29 15:58:01 +0200

    scenarios page structured

--- Website/server.py
+++ Website/server.py
@@ -21,6 +21,35 @@ import secrets
 
 app = Flask(__name__)
 
+hazards = [0.2,0.4,0.6,0.8]
+organismsForGravity = {
+    'human': 4.8*hazards[0],
+    'mice': 4*hazards[0],
+    'rat': 3.5*hazards[0]
+}
+organismsForRadiation = {
+    'dr': 2.4*hazards[1],
+    'tard':2.1*hazards[1],
+    'human':1.85*hazards[1]
+}
+organismsForDna = {
+    'tard' : 1.55 * hazards[2],
+    'nmr': 1.3* hazards[2],
+    'mice':1.1* hazards[2]
+}
+organismsForeye = {
+    'rbs': 1.2* hazards[3],
+    'human':1* hazards[3],
+    'eagle':0.8* hazards[3]
+}
+probabilities  = []
+for keyG,valG in organismsForGravity.items():
+    for keyR,valR in organismsForRadiation.items():
+        for keyD,valD in organismsForDna.items():
+            for keyE,valE in organismsForeye.items():
+                probabilities.append(valG * valR * valD * valE)
+
+
 # ----------------------------------------------------------------------------index-----------------------------------------------------------
 @app.route('/')
 @app.route('/home',methods=["GET","POST"])
@@ -41,12 +70,27 @@ def mainMenu():
 
 @app.route('/mainMenu/GO',methods=['GET','POST'])
 def GO():
-    attr1 = float(request.form['attr1'])
-    attr2 = float(request.form['attr2'])
-    attr3 = float(request.form['attr3'])
-    attr4 = float(request.form['attr4'])
+    attr1 = float(request.form['attr1'])*hazards[0]
+    attr2 = float(request.form['attr2'])*hazards[1]
+    attr3 = float(request.form['attr3'])*hazards[2]
+    attr4 = float(request.form['attr4'])*hazards[3]
+    destination = attr1 * attr2 * attr3 * attr4
+    print(destination)
     
-    return render_template('go.html',destination = attr1 * attr2 * attr3 * attr4)
+    if(destination > 0.82):
+        scenario = 6
+    elif destination > 0.6:
+        scenario = 5
+    elif destination > 0.5:
+        scenario = 4
+    elif destination > 0.4:
+        scenario = 3
+    elif destination > 0.3:
+        scenario = 2
+    elif destination > 0.2:
+        scenario = 1
+    return render_template('scenarios.html',destination = destination,scenario = scenario,attr1 = float(request.form['attr1']),attr2 = float(request.form['attr2'])
+                           ,attr3 = float(request.form['attr3']),attr4 = float(request.form['attr4']))
     
 
 if __name__ == '__main__':
--- Website/trial.py
+++ Website/trial.py
@@ -1,11 +1,35 @@
 # import numpy as np
+import matplotlib.pyplot as plt
 
-# hazards = ['frst','scnd','thrd','frth']
-# hazard_intensity = np.arrany([1,2,3,4])*0.01
-
-# features = ['frst','scnd','thrd','frth']
-# features_endurance = {'frst_creature' : [1,2,5,2],
-#                       'scnd_creature' : [1.5,2.4,3.1,4],
-#                       'thrd_creature' : [1,2,3,4],
-#                       'frth_creature' : [1,2,3,4],
-#                       }
+hazards = [0.2,0.4,0.6,0.8]
+organismsForGravity = {
+    'human': 4.8*hazards[0],
+    'mice': 4*hazards[0],
+    'rat': 3.5*hazards[0]
+}
+organismsForRadiation = {
+    'dr': 2.4*hazards[1],
+    'tard':2.1*hazards[1],
+    'human':1.85*hazards[1]
+}
+organismsForDna = {
+    'tard' : 1.55 * hazards[2],
+    'nmr': 1.3* hazards[2],
+    'mice':1.1* hazards[2]
+}
+organismsForeye = {
+    'rbs': 1.2* hazards[3],
+    'human':1* hazards[3],
+    'eagle':0.8* hazards[3]
+}
+probabilities  = []
+for keyG,valG in organismsForGravity.items():
+    for keyR,valR in organismsForRadiation.items():
+        for keyD,valD in organismsForDna.items():
+            for keyE,valE in organismsForeye.items():
+                probabilities.append(valG * valR * valD * valE)
+                
+                
+print(probabilities)
+plt.hist(probabilities)
+plt.show()
